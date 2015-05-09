d6df29aa6f68605bdfd6ba6f235f1f706c6dc9ab
Bart Callant <bart@callant.net>
2015-05-09 20:26:51 +0200

    Turn on LED

--- RaspberryPi.Windows10.LEDs/MainPage.xaml.cs
+++ RaspberryPi.Windows10.LEDs/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,48 @@ namespace RaspberryPi.Windows10.LEDs
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int LEDPIN = 5;
+        private GpioPin led = null;
+        private GpioController gpio = null;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Unloaded += MainPage_Unloaded;
+
+            InitGPIO();
+
+            TurnLedOn(ref led, LEDPIN);
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            led.Dispose();
+        }
+
+        public void InitGPIO()
+        {
+            gpio = GpioController.GetDefault();
+
+            if(gpio == null)
+            {
+                return;
+            }
+        }
+
+        public void TurnLedOn(ref GpioPin pin, int pinNumber)
+        {
+            pin = gpio.OpenPin(pinNumber);
+
+            if (pin == null)
+            {
+                return;
+            }
+
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            // Pin needs to be low in order to go on
+            pin.Write(GpioPinValue.Low);
         }
     }
 }
